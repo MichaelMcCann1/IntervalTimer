a01fe7f3263d090a2a67a699b472dbef93b6c537
Michael McCann <mmccann0903@gmail.com>
2022-08-23 21:50:16 -0500

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import WorkoutSetup from "./routes/WorkoutSetup";
 import { GlobalStyle } from "./components/GlobalStyles/GlobalStyles";
 import { RecoilRoot } from "recoil";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <RecoilRoot>
       <GlobalStyle />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/workout-options" element={<WorkoutSetup />} />
+      </Routes>
     </RecoilRoot>
   );
 }
